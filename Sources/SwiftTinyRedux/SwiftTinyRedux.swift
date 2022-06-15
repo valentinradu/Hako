972c9d0616be754da2be192a72c4658cc3586ecd
Valentin Radu <radu.v.valentin@me.com>
2022-06-15 21:13:44 +0100

    Made state public in store

--- Sources/SwiftTinyRedux/SwiftTinyRedux.swift
+++ Sources/SwiftTinyRedux/SwiftTinyRedux.swift
@@ -75,7 +75,7 @@ public struct Store<S, E>: Dispatcher {
                                  environment: environmentKeyPath)
     }
 
-    var state: S {
+    public var state: S {
         _underlyingStore.state
     }
 
