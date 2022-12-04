2d6fb898d017292c090f27c8504e4533abb31972
Valentin Radu <radu.v.valentin@me.com>
2022-12-04 14:36:36 +0000

    Moved to simple type mutations and side effects

--- Sources/Hako/Hako.swift
+++ Sources/Hako/Hako.swift
@@ -7,24 +7,118 @@
 
 import Foundation
 
-public protocol Mutation<S> where S: Equatable {
-    associatedtype S: Equatable
-    func reduce(state: inout S) -> Command<S>
-}
+/// Mutations are operations that trigger a state update in the store.
+/// They are resolved to pure functions that receive the state, apply
+/// a transformation on it then return any side effects that might need
+/// to be performed further.
+///
+/// Mutations drive the functional core of the store and are always
+/// performed serially on the main thread.
+public protocol Mutation: Hashable {}
 
-public protocol SideEffect<S> where S: Equatable {
-    associatedtype S: Equatable
-    func perform() async -> Command<S>
-}
+/// Side effects are operations that happen outside of the functional core
+/// of the store and ultimately lead to a mutation.
+/// Side effects are driving the imperative shell of the store. They're
+/// resolved to async `Task<Mutation, Error>`s and can access and modify
+/// the environment.
+public protocol SideEffect: Hashable {}
 
-public indirect enum Command<S> where S: Equatable {
-    case noop
-    case perform(any SideEffect<S>)
-    case reduce(any Mutation<S>)
-    case dispatch(strategy: DispatchStrategy, commands: [Command<S>])
-}
+public enum ConcurrencyStrategy: Hashable {
+    public enum Priority: Hashable {
+        case medium
+        case high
+        case low
+    }
 
-public enum DispatchStrategy {
+    /// Execute each side effect one after the other
     case serial
-    case concurrent
+    /// Execute side effects concurrently. Optionally set priority
+    case concurrent(priority: Priority = .medium)
+}
+
+extension ConcurrencyStrategy.Priority {
+    var taskPriority: TaskPriority {
+        switch self {
+        case .high:
+            return .high
+        case .low:
+            return .low
+        case .medium:
+            return .medium
+        }
+    }
+}
+
+/// Commands control how mutations and side effects are performed
+public indirect enum SideEffectCommand: Hashable {
+    case noop
+    case perform(sideEffect: any SideEffect)
+    case merge(strategy: Strategy = .serial,
+               whenDone: MutationCommand = .noop,
+               commands: [SideEffectCommand])
+
+    public static func == (lhs: SideEffectCommand, rhs: SideEffectCommand) -> Bool {
+        switch (lhs, rhs) {
+        case (.noop, .noop):
+            return true
+        case let (.perform(lhsMutation), .perform(rhsMutation)):
+            return AnyHashable(lhsMutation) == AnyHashable(rhsMutation)
+        case let (.merge(lhsStrategy, lhsWhenDone, lhsCommands), .merge(rhsStrategy, rhsWhenDone, rhsCommands)):
+            return AnyHashable(lhsCommands) == AnyHashable(rhsCommands)
+                && AnyHashable(lhsStrategy) == AnyHashable(rhsStrategy)
+                && AnyHashable(lhsWhenDone) == AnyHashable(rhsWhenDone)
+        default:
+            return false
+        }
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        switch self {
+        case .noop:
+            hasher.combine(0)
+        case let .perform(mutation):
+            hasher.combine(1)
+            hasher.combine(AnyHashable(mutation))
+        case let .merge(strategy, whenDone, commands):
+            hasher.combine(2)
+            hasher.combine(commands)
+            hasher.combine(strategy)
+            hasher.combine(whenDone)
+        }
+    }
+}
+
+public indirect enum MutationCommand: Hashable {
+    case noop
+    case perform(mutation: any Mutation)
+    case merge(strategy: Strategy = .serial,
+               commands: [MutationCommand])
+
+    public static func == (lhs: MutationCommand, rhs: MutationCommand) -> Bool {
+        switch (lhs, rhs) {
+        case (.noop, .noop):
+            return true
+        case let (.perform(lhsMutation), .perform(rhsMutation)):
+            return AnyHashable(lhsMutation) == AnyHashable(rhsMutation)
+        case let (.merge(lhsStrategy, lhsCommands), .merge(rhsStrategy, rhsCommands)):
+            return AnyHashable(lhsCommands) == AnyHashable(rhsCommands)
+                && AnyHashable(lhsStrategy) == AnyHashable(rhsStrategy)
+        default:
+            return false
+        }
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        switch self {
+        case .noop:
+            hasher.combine(0)
+        case let .perform(mutation):
+            hasher.combine(1)
+            hasher.combine(AnyHashable(mutation))
+        case let .merge(strategy, commands):
+            hasher.combine(2)
+            hasher.combine(commands)
+            hasher.combine(strategy)
+        }
+    }
 }
--- Sources/Hako/Store.swift
+++ Sources/Hako/Store.swift
@@ -33,79 +33,93 @@ public extension Store {
     }
 }
 
-// MARK: Ingesting commands
-
 public extension Store {
-    func ingest(_ publisher: any Publisher<Command<S>, Never>) {
-        precondition(Thread.isMainThread, "\(#function) should always be called on the main thread")
-
-        publisher.sink { [weak self] in
-            guard let self = self else { return }
-            self.dispatch($0)
-        }
-        .store(in: &_cancellables)
-    }
-}
-
-public extension Store {
-    func dispatch(_ command: Command<S>) {
+    func perform(command: SideEffectCommand) async throws -> [any Mutation] {
         switch command {
         case .noop:
             break
         case let .perform(sideEffect):
-            withManagedTask { [weak self] in
-                guard let self = self else { return }
-                await self.perform(sideEffect)
+            let mutation = try await perform(sideEffect: sideEffect)
+            return [mutation]
+        case let .merge(strategy, whenDone, commands):
+            switch strategy {
+            case .serial:
+                var mutations: [any Mutation] = []
+                for command in commands {
+                    let others = try await perform(command: command)
+                    mutations.append(contentsOf: others)
+                }
+                mutations.append(whenDone)
+                return mutations
+            case let .concurrent(priority):
+                var mutations = try await withThrowingTaskGroup(of: [any Mutation]?.self,
+                                                                returning: [any Mutation].self) { [weak self] group in
+                    for command in commands {
+                        group.addTask(priority: priority.taskPriority) { [weak self] in
+                            try await self?.perform(command: command)
+                        }
+                    }
+
+                    var mutations: [any Mutation] = []
+                    while let result = await group.nextResult() {
+                        switch result {
+                        case .failure(error):
+                            break
+                        case let .success(other):
+                            if let other {
+                                mutations.append(contentsOf: other)
+                            }
+                        }
+                    }
+
+                    return mutations
+                }
+
+                mutations.append(whenDone)
+                return mutations
             }
-        case let .dispatch(strategy, commands):
-            withManagedTask { [weak self] in
-                guard let self = self else { return }
-                await self.dispatch(commands, strategy: strategy)
-            }
-        case let .reduce(mutation):
-            reduce(mutation)
         }
     }
 
-    func dispatch(_ commands: [Command<S>], strategy: DispatchStrategy) async {
-        switch strategy {
-        case .serial:
-            for command in commands {
-                dispatch(command)
-            }
-        case .concurrent:
-            await withTaskGroup(of: Void.self) { [weak self] group in
-                for command in commands {
-                    group.addTask { [weak self] in
-                        guard let self = self else { return }
-                        await self.dispatch(command)
-                    }
-                }
+//    func dispatch(_ command: MutationCommand) {
+//        switch command {
+//        case .noop:
+//            break
+//        case let .perform(mutation):
+//            reduce(mutation)
+//        case .merge(strategy, commands):
+//            for command in commands {
+//                reduce(comm)
+//            }
+//        }
+//    }
+}
 
-                await group.waitForAll()
-            }
+// MARK: Ingesting commands
+
+public extension Store {
+    func ingest(_ publisher: any Publisher<SideEffectCommand, Never>) {
+        precondition(Thread.isMainThread, "\(#function) should always be called on the main thread")
+
+        publisher.sink { [weak self] in
+            self?.dispatch($0)
         }
+        .store(in: &_cancellables)
     }
 }
 
 // MARK: Dispatching actions
 
 private extension Store {
-    func reduce(_ mutation: any Mutation<S>) {
+    func reduce(mutation: any Mutation) -> any SideEffect {
         precondition(Thread.isMainThread, "\(#function) should always be called on the main thread")
-
-        let command = mutation.reduce(state: &_state)
-        dispatch(command)
     }
 }
 
 // MARK: Performing side effects
 
 private extension Store {
-    nonisolated func perform(_ sideEffect: any SideEffect<S>) async {
-        let command = await sideEffect.perform()
-        await dispatch(command)
-    }
+    nonisolated func perform(sideEffect: any SideEffect) async throws -> any Mutation {}
 }
 
 // MARK: Other utils
@@ -118,9 +132,8 @@ private extension Store {
     func withManagedTask(_ run: @escaping () async -> Void) {
         let uuid = UUID()
         let task = Task.detached { [weak self] in
-            guard let self = self else { return }
             await run()
-            await self.removeTask(uuid: uuid)
+            await self?.removeTask(uuid: uuid)
         }
         _tasks[uuid] = task
     }
